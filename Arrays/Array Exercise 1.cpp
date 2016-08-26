ae46fd3e404993566bd647f7fcd7efa8472eeed0
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-26 14:41:22 -0500

    Add files via upload

---
+++ Arrays/Array Exercise 1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int main()
+{
+//	int numbers[10] = { 0,0,1,0,0,1,0,0,1,1 };
+//	std::cout << numbers[10];
+//	Valid
+
+//	int matrix[5] = { 1, 2, 3, 4, 5, 6, 7 };
+//	Invalid: Too many integers inside "{}"
+
+//	int radii[10] = (3.2, 4.7};
+//	Invalid: The "(" instead of "{"
+
+//	int table[7] = { 2, , ,27, ,45, 39 };
+//	Invalid: No expression inside "{}" in blank spaces
+
+//	char codes[] = { 'A', 'X', '1', '2', 'S' };
+//	std::cout << codes[];
+//	Invalid: No expression in "[]"
+
+//	int blanks[];
+//	std::cout << blanks[];
+//	Invalid: Incomplete type 
+
+//	int collection[-20];
+//	Invalid: Array needs to be bigger than 0
+
+//	int hours[3] = 8, 12, 16 ;
+//	Invalid: Does not have "{}"
+
+//	printf("%i", codes);
+
+	int values[] = { 2, 6, 10, 14 };
+	int x = 2;
+	std::cout << values[4];
+
+	system("pause");
+}
