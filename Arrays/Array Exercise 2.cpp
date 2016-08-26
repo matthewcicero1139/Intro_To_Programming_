38becd82478e3f11e8ee4d7a01093ad0033338b3
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-26 14:43:36 -0500

    Add files via upload

---
+++ Arrays/Array Exercise 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+
+int values[] = { 2, 6, 10, 14 };
+
+//	std::cout << values[2];
+
+//	std::cout << ++values[0];
+
+//	std::cout << values[1]++;
+
+//int x = 2;
+//std::cout << values[++x];
+
+//std::cout << values[4];
+
+system("pause");
+}
---
+++ Arrays/Array Exercise 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+	int m = 11;
+	int k = -1;
+	int data[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, };
+
+	for (int data[10]; data[10] < m; data[10]++)
+	{
+	
+		std::cout << data[++k];
+		
+	}
+//	for (int data[10]; data[10] < k; data[10]++)
+//	{
+//		std::cout << data[10] << std::endl;
+		system("pause");
+//	}
+}
