5fc896c367229be605e8d0eeed1e95c01d157549
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-31 12:50:14 -0500

    Add files via upload

---
+++ Conditional/Conditional Exercise 3.cpp
@@ -0,0 +1,43 @@
+// Working better than what I had before.
+
+#include <iostream>
+int main()
+{
+	//Creating integer points
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+
+	//Allowing the input of user
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> d;
+	std::cin >> e;
+
+//	if num is > 0 print nums in ascending order
+//		if num is < 0 print nums in descending order
+//			if num is = 0 print nums in order input
+
+//	std::cout << a + b + c + d + e << std::endl;
+	int k = a + b + c + d + e;
+
+	//Not outputting is ascending or descending order, only the order it was input
+
+	if (k > 0)
+	{
+		std::cout << a << b << c << d << e << std::endl;
+	}
+	else if (k < 0)
+	{
+		std::cout << e << d << c << b << a << std::endl;
+	}
+	else (k == 0);
+	{
+		std::cout << a << b << c << d << e << std::endl;
+	}
+
+	system("pause");
+}
