b9f09045c5c966e22034a561d310391d93faa247
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-08 09:15:24 -0500

    Update Array Exercise 3.cpp

--- Arrays/Array Exercise 3.cpp
+++ Arrays/Array Exercise 3.cpp
@@ -1,19 +1,11 @@
+// Write a loop for array data[10]
 #include <iostream>
 int main()
 {
-	int m = 11;
-	int k = -1;
-	int data[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, };
+	int data[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
-	for (int data[10]; data[10] < m; data[10]++)
-	{
-	
-		std::cout << data[++k];
-		
-	}
-//	for (int data[10]; data[10] < k; data[10]++)
-//	{
-//		std::cout << data[10] << std::endl;
-		system("pause");
-//	}
+	for (int data = 10; data > 0; --data)
+		std::cout << data << std::endl;
+
+	system("pause");
 }
