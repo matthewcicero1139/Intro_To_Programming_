adc8196350c154a5e244fe5189169eff6b12fd13
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-08 16:00:14 -0500

    Add files via upload

---
+++ Swap/Students.cpp
@@ -0,0 +1,41 @@
+// NOT FINISHED
+// swap the students
+#include <iostream>
+//class
+// Need an array to put students into allowing them to swap
+// does the array have to go under the main or the class
+// need to name students classes thingy
+// something needs to go into public but i dont know what
+// use m instead of k
+
+class student
+{
+	private:
+		int m_age;
+		char m_sex;
+		char m_name;
+		float m_height;
+
+public:
+	student() {}
+	student(int a, char s, char n, float h)
+	{
+		m_age = a;
+		m_sex = s;
+		m_name = n;
+		m_height = h;
+
+	}
+
+};
+
+bool swap(student &a, student &b)
+
+// need student array
+
+
+
+; int main()
+{
+	student Dylan = student(12, 'M', 'D', 6);
+}
