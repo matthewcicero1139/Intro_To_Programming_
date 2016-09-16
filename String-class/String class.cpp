d4b3ff6cb4773c9c5cd23802a27969b04dc2c14c
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-16 14:16:56 -0500

    Add files via upload

---
+++ String-class/String class.cpp
@@ -0,0 +1,55 @@
+// ask strings length and return value
+
+// need a class
+//class; construcer; members
+class String
+{
+public:
+	String() {};
+	String(char s) {};
+
+private:
+	int m_cic;
+	char m_k[10];
+
+};
+
+//use class to query a strings length
+
+
+
+#include <iostream>
+int main()
+{//need function asking about strings length
+	// function querying string length
+
+	//int sL(char ria);
+	//{
+	//	char ria[10] = "irish";//should be 5
+	//	strlen(ria);
+	//	int ish = strlen(ria);
+	//	return ish;
+	//}
+
+	//access a character in a certain index within the string class
+
+
+
+
+
+
+
+
+
+
+	String abc = String();
+	String abcwithchar = String('f');
+	char d;
+	
+	int b;
+}
+
+int func()
+{
+
+}
