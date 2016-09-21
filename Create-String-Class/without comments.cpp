c173961e27d8b135c562cc6a17302f43a193a0b9
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-21 18:07:47 -0500

    Add files via upload

--- Create-String-Class/without comments.cpp
+++ Create-String-Class/without comments.cpp
@@ -1,13 +1,20 @@
 
 #include <iostream>
+// make struct that makes MyString equal to char\
+cant  make it equal directly. need a way to: swap? maybe\
+make them equal to something else then equal the things they are else, equal to the oposites.
+
+struct a
+{
+	MyString a;
+	char b;
+
+};
 
 class MyString
 {
-	// making constructor
-	struct test {
-		int a;
-		char b; 
-	};
+	// make Struct that makes mystring equal to char
+	
 
 
 	// declaring functions first.
@@ -55,9 +62,13 @@ int main()
 	MyString queryLength;
 	query.queryLength();
 	return 0;
+	
 }
 
 // What im doing now:
-// trying to make constructor that will convert MyString into type char
+// trying to make constructor that will convert MyString into type char 
 
-// may need to turn some things already there into structs
+// may need to turn some things already there into structs 
+
+/* ok so structs will probably help with the error about having to convert constructor 
+it also has information about dot operator*/ 
