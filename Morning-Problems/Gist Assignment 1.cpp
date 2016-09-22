a60ad610402376ac4776ed143a929f68d684495b
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-22 11:14:47 -0500

    Add files via upload

---
+++ Morning-Problems/Gist Assignment 1.cpp
@@ -0,0 +1,48 @@
+//// NOT FINISHED
+//
+////Gist Assignment: Create a function that takes in a character array\
+//and reverses the string stored inside of it. Due by 1000.\
+//Submit in the form of a gist.
+//
+//#include <iostream>
+//// declaring function
+//char function(char car[20])
+//{
+//	return 0;
+//}
+//
+//int main()
+//{
+//	// making character array
+//	char car[20] = { "hunter" };
+//
+//	// making function to reverse
+//	char function(char car[20]);
+//	{
+//	
+//	
+//
+//	}
+//}
+//
+//// Descripton: I dont know how to begin makeing the reverse\
+//	Name: Gist Assignment
+
+#include <iostream>
+
+int main()
+{
+	// make function
+	void alps();
+	{
+		char todd[] = "cats";
+
+	}
+
+
+}
+
+// List for me to write what i dont know or understand.
+/* 1. i dont understand functions. how do you set them up as in how
+do you declare or identify or define them whats the difference between
+identifing declarind and defining*/
