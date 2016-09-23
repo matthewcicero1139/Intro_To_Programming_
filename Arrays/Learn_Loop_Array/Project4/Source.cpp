662a0a92ac70c4acbc6daa65fb662f9a39ff13ad
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-23 17:01:45 -0500

    Add files via upload

---
+++ Arrays/Learn_Loop_Array/Project4/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+	int myArray[5] = { 3,2,8,9,1 };
+	
+	//Making function
+	int myFunc(myArray[5]);
+	{
+	for (int k : myArray)
+		{
+			for (int k = 0; k < 5; ++k);
+			{
+				std::cout << myArray[k] << std::endl;
+				system("pause");
+			}
+		}
+	}
+}
