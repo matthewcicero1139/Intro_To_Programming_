41a5a3b021c1108c9327a5004eece1f93d1400e2
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-23 10:14:28 -0500

    Add files via upload

---
+++ Testing_upload_github/Morning Problem 3/Morning Problem 3/Morning_Problem_3.cpp
@@ -0,0 +1,28 @@
+// Morning assignment: Create a function that takes in a array of integers and the size of the array.
+// Return the sum of the two largest values in the array.
+
+#include <iostream>
+int main()
+{
+	// The function
+	int func();
+	{
+		// The array
+		int cat[5] = { 3, 5, 2, 1, 9 };
+		//lagest values are at 1 and 4
+		// loop through array
+		// for loop while looping through pull largest numbers out of index and assign them to large1 and large2
+		// 
+
+		// Find size of array
+		// Dont know how...
+
+		// Assigning 2 largest values to large1 and large2
+		= large1;
+		= large2;
+
+		// Returning sum of two largest numbers
+		return large1 + large2;
+	}
+
+}
