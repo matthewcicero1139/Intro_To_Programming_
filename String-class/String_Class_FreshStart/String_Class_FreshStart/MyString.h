676d30caeed912a84de0380386ea06ec5994942c
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-29 13:19:26 -0500

    Add files via upload

---
+++ String-class/String_Class_FreshStart/String_Class_FreshStart/MyString.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//class going in here along with all function declarations
+
+class MyString
+{
+	char* m_string;
+
+
+public:
+	// need constructors
+	//empty construcor
+	MyString(){}
+	//constructor with pointer
+	MyString(char m_string) {};
+
+	// trying to use pointer
+	MyString::MyString*m_string;
+
+	//what counts the strings length? how do i point to it
+
+};
+
+
---
+++ String-class/String_Class_FreshStart/String_Class_FreshStart/main.cpp
@@ -0,0 +1,11 @@
+// main function
+
+#include <iostream>
+#include "MyString.h"
+
+int main()
+{
+	// array goes here
+	char ary[10] = { "dog" };
+	
+}
