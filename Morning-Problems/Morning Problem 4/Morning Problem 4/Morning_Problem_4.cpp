0c7f338afa82262271929154a7e572f3cd9d5f9a
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-30 10:07:34 -0500

    Add files via upload

---
+++ Morning-Problems/Morning Problem 4/Morning Problem 4/Morning_Problem_4.cpp
@@ -0,0 +1,46 @@
+
+//Morning Assignment : Create a function that takes in two arguments,
+//One that points to an array of integers and
+//one for the size of the array.This function will
+//return the value in the array that appears
+//the most times.
+
+#include <iostream>
+
+//do i need pointer just pointing at size?
+
+// int array
+int myArray[5] = { 1, 5, 6, 5, 3 };
+// making pointer and pointing to myArray
+int* myPointer = myArray;
+//int for the loop
+int a;
+
+// making function
+int myFunc(int* myPointer, int size)
+{
+
+	//NOT NEEDED already known what the size is because it is already defined
+
+//// making loop that will find size of array that is type void
+//	for (size = 0; size < a ; size++);
+//	{
+//		
+//	}
+
+
+}
+
+int main()
+{
+
+}
+
+//what i have:
+//1. an array and a pointer that points to the array
+//2. a function that takes the pointer to the array and the size of array in its arguments
+
+//what i do not have:
+//1. my loop finding the size of the array is not finished
+//2. not finding the one that is repeated in the array
+//3. returning nothing
