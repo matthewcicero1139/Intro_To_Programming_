062b4a9d2617e9b1e2cbc09200262afa25809907
matthewcicero1139 <matthewcicero33@gmail.com>
2016-09-30 16:03:25 -0500

    Add files via upload

---
+++ Strings-Class-fresh-start/String_Class_FreshStart/String_Class_FreshStart/MyString.cpp
@@ -0,0 +1,6 @@
+#include "MyString.h"
+
+// Declaration of function in header file
+MyString::MyString()
+{
+}
---
+++ Strings-Class-fresh-start/String_Class_FreshStart/String_Class_FreshStart/main.cpp
@@ -0,0 +1,11 @@
+// main function
+
+#include <iostream>
+#include "MyString.h"
+
+int main()
+{
+	// need the string that the array needs here.
+	// make m_string = { "ohm "}
+	MyString m_string = { "ohm" };
+}
